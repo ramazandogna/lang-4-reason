466a53f5b2d71f69bdf957ae96c44a9e997bba31
Ramazan DOĞAN <doganrmzn40@gmail.com>
2025-06-12 00:55:57 +0300

    feat: add post images export module

---
+++ src/images/post-images/index.ts
@@ -0,0 +1,21 @@
+import asset1 from './asset-1.png';
+import asset2 from './asset-2.jpg';
+import asset3 from './asset-3.jpg';
+import asset4 from './asset-4.jpg';
+import asset5 from './asset-5.jpg';
+import asset6 from './asset-6.jpg';
+import asset7 from './asset-7.jpg';
+import asset8 from './asset-8.jpg';
+import asset9 from './asset-9.png';
+
+export const postImages = [
+  asset1,
+  asset2,
+  asset3,
+  asset4,
+  asset5,
+  asset6,
+  asset7,
+  asset8,
+  asset9
+];
