21ba7dcdb2120fb53e3a15ae6f7cb40c1cace0fd
Ramazan DOĞAN <doganrmzn40@gmail.com>
2025-06-12 00:55:44 +0300

    feat: add avatar images export module

---
+++ src/images/avatar-images/index.ts
@@ -0,0 +1,21 @@
+import avatar1 from './avatar-1.png';
+import avatar2 from './avatar-2.png';
+import avatar3 from './avatar-3.png';
+import avatar4 from './avatar-4.png';
+import avatar5 from './avatar-5.png';
+import avatar6 from './avatar-6.png';
+import avatar7 from './avatar-7.png';
+import avatar8 from './avatar-8.png';
+import avatar9 from './avatar-9.png';
+
+export const avatarImages = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9
+];
