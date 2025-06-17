926d0031cd91cd82563e24867dd84267b76daf4c
Ramazan DOĞAN <doganrmzn40@gmail.com>
2025-06-17 19:31:26 +0300

    Section had added

--- src/layouts/PostDetail/Content/Content.tsx
+++ src/layouts/PostDetail/Content/Content.tsx
@@ -1,84 +1,87 @@
 import Image from 'next/image';
 import { ContentProps } from './Content.types';
 import { getBlurData } from '@/lib/imageUtilis';
+import { Section } from '@/components/ui/Section';
 
 export default function Content({ post }: { post: ContentProps }) {
   const avatarBlurDataURL = getBlurData(post.avatar);
 
   return (
-    <article className="mx-auto max-w-4xl px-4 pt-10 pb-20">
-      <div className="mb-4 text-sm font-semibold text-indigo-600">
-        {post.category}
-      </div>
-      <h1 className="mb-6 text-4xl font-extrabold text-gray-900">
-        {post.title}
-      </h1>
-      <p className="mb-10 text-lg text-gray-700">
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-        {post.desc}
-      </p>
-
-      <div className="flex items-center gap-3">
-        <Image
-          src={post.avatar}
-          alt={post.author || 'Author image'}
-          width={48}
-          height={48}
-          className="rounded-full"
-          placeholder="blur"
-          blurDataURL={avatarBlurDataURL}
-        />
-        <div className="text-sm">
-          <div className="font-semibold">{post.author}</div>
-          <div className="text-gray-400">{post.readTime}</div>
+    <Section className="pt-0!">
+      <article className="mx-auto max-w-4xl px-4 pt-10 pb-20">
+        <div className="mb-4 text-sm font-semibold text-indigo-600">
+          {post.category}
         </div>
-      </div>
-    </article>
+        <h1 className="mb-6 text-4xl font-extrabold text-gray-900">
+          {post.title}
+        </h1>
+        <p className="mb-10 text-lg text-gray-700">
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+          {post.desc}
+        </p>
+
+        <div className="flex items-center gap-3">
+          <Image
+            src={post.avatar}
+            alt={post.author || 'Author image'}
+            width={48}
+            height={48}
+            className="rounded-full"
+            placeholder="blur"
+            blurDataURL={avatarBlurDataURL}
+          />
+          <div className="text-sm">
+            <div className="font-semibold">{post.author}</div>
+            <div className="text-gray-400">{post.readTime}</div>
+          </div>
+        </div>
+      </article>
+    </Section>
   );
 }
--- src/layouts/PostDetail/Hero/Hero.tsx
+++ src/layouts/PostDetail/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import { ContentHeroProps } from '../Content/Content.types';
 import HeroTitle from './HeroTitle/HeroTitle';
 import HeroImage from './HeroImage/HeroImage';
 import { HeroImageProps, HeroTitleProps } from './Hero.types';
+import { Section } from '@/components/ui/Section';
 
 function Hero({ hero }: { hero: ContentHeroProps }) {
   const heroImage: HeroImageProps = {
@@ -19,10 +20,10 @@ function Hero({ hero }: { hero: ContentHeroProps }) {
   };
 
   return (
-    <div className="flex flex-col gap-16 max-md:gap-10">
+    <Section className="flex flex-col gap-16 pt-24 pb-16 max-md:gap-10 max-md:pt-16 max-md:pb-10">
       <HeroTitle hero={heroTitle} />
       <HeroImage hero={heroImage} />
-    </div>
+    </Section>
   );
 }
 
