9a3211d465fff7c79a0a46531b628531f81122b8
Ramazan DOĞAN <doganrmzn40@gmail.com>
2025-07-02 22:49:53 +0300

    removed loading component

--- src/app/[slug]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Section } from '@/components/ui/Section';
-
-export default function Loading() {
-  return (
-    <Section className="p-8 text-center">
-      <p>Yükleniyor...</p>
-    </Section>
-  );
-}
