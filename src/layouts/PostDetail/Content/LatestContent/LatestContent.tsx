23ea62869d9cd41ef3f58d842699e18ef405d90e
Ramazan DOĞAN <doganrmzn40@gmail.com>
2025-07-03 12:19:23 +0300

    Remove unused image blur handling from LatestContent component

--- src/layouts/PostDetail/Content/LatestContent/LatestContent.tsx
+++ src/layouts/PostDetail/Content/LatestContent/LatestContent.tsx
@@ -4,18 +4,8 @@ import getAllPosts from '@/data/getAllPosts';
 // Layouts
 import { LatestPosts } from '@/layouts/Home/LatestPosts';
 
-// Utils
-import { generateBlurDataURL } from '@/utils/plaiceholder';
-
 export default async function LatestContent() {
   const posts = await getAllPosts('', null, 3);
 
-  // İlk post'un featured image'ı için blur oluştur
-  const firstPostImage =
-    posts.nodes[0]?.featuredImage?.node?.mediaDetails?.sizes?.[0]?.sourceUrl;
-  const blurDataURL = firstPostImage
-    ? await generateBlurDataURL(firstPostImage)
-    : undefined;
-
-  return <LatestPosts posts={posts} blurDataURL={blurDataURL} />;
+  return <LatestPosts posts={posts} />;
 }
